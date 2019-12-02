9b589c24ac786a20bffdd373412f946392e991b7
Kunping Lin <kunping2@illinois.edu>
2019-12-02 13:54:47 -0600

    update: main Ui v1.1

--- app/src/main/java/com/example/gyrododger/GameActivity.java
+++ app/src/main/java/com/example/gyrododger/GameActivity.java
@@ -13,8 +13,8 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Button end = findViewById(R.id.end);
-        end.setOnClickListener(unused -> {
+        Button playAgain = findViewById(R.id.playAgain);
+        playAgain.setOnClickListener(unused -> {
             finish();
         });
     }
