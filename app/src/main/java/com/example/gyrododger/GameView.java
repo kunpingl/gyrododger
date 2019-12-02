83046f3c9e79e56b3a4338cc3863ede95dcbfe16
kaiyuan8 <kaiyuan8@illinois.edu>
2019-12-02 17:37:12 -0600

    game view added

---
+++ app/src/main/java/com/example/gyrododger/GameView.java
@@ -0,0 +1,29 @@
+package com.example.gyrododger;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class GameView extends View {
+
+    private Bitmap player;
+    private Bitmap backgroundImage;
+
+    public GameView(Context context) {
+        super(context);
+        Bitmap temp_player = BitmapFactory.decodeResource(getResources(), R.drawable.player);
+        player = Bitmap.createScaledBitmap(
+                temp_player, 300, 300, false);
+
+        backgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.universe);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(player, 0, 0,null);
+        canvas.drawBitmap(backgroundImage, 0,0,null);
+    }
+}
--- app/src/main/java/com/example/gyrododger/StartActiviy.java
+++ app/src/main/java/com/example/gyrododger/StartActiviy.java
@@ -14,7 +14,7 @@ public class StartActiviy extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         Button start = findViewById(R.id.start);
         start.setOnClickListener(ununsed -> {
-            startActivityForResult(new Intent(this, EndActivity.class),1);
+            startActivityForResult(new Intent(this, GameActivity.class),1);
         });
     }
 }
