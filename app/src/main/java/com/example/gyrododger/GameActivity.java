a43a74aaca8e039e65f38f47ec3c8b067aa581d5
Kunping Lin <kunping2@illinois.edu>
2019-12-02 23:40:12 -0600

    update: Moving Flight done

--- app/src/main/java/com/example/gyrododger/GameActivity.java
+++ app/src/main/java/com/example/gyrododger/GameActivity.java
@@ -1,75 +1,109 @@
 package com.example.gyrododger;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+
+import java.lang.reflect.Field;
+
 
 public class GameActivity extends Activity {
 
-    private ViewGroup mainLayout;
-    private ImageView image;
+    private int screenWidth;
+    private int screenHeight;
+    private ImageView moveIv;
 
-    private int xDelta;
-    private int yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        mainLayout = (RelativeLayout) findViewById(R.id.main);
-        image = (ImageView) findViewById(R.id.image);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels - 50;
 
-        image.setOnTouchListener(onTouchListener());
-    }
-
-    private OnTouchListener onTouchListener() {
-        return new OnTouchListener() {
-
-            @SuppressLint("ClickableViewAccessibility")
+        moveIv = (ImageView) findViewById(R.id.flight);
+        moveIv.setOnTouchListener(movingEventListener);
+        moveIv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent event) {
-
-                final int x = (int) event.getRawX();
-                final int y = (int) event.getRawY();
-
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-
-                    case MotionEvent.ACTION_DOWN:
-                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
-                                view.getLayoutParams();
-
-                        xDelta = x - lParams.leftMargin;
-                        yDelta = y - lParams.topMargin;
-                        break;
-
-                    case MotionEvent.ACTION_UP:
-                        Toast.makeText(GameActivity.this,
-                                "Wooooo!", Toast.LENGTH_SHORT)
-                                .show();
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
-                                .getLayoutParams();
-                        layoutParams.leftMargin = x - xDelta;
-                        layoutParams.topMargin = y - yDelta;
-                        layoutParams.rightMargin = 0;
-                        layoutParams.bottomMargin = 0;
-                        view.setLayoutParams(layoutParams);
-                        break;
-                }
-
-                mainLayout.invalidate();
-                return true;
+            public void onClick(View v) {
+                // click event
             }
-        };
+        });
     }
+
+
+    private OnTouchListener movingEventListener = new OnTouchListener() {
+        int lastX, lastY, x, y;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    lastX = (int) event.getRawX();
+                    lastY = (int) event.getRawY();
+                    x = (int) event.getRawX();
+                    y = (int) event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int dx = (int) event.getRawX() - lastX;
+                    int dy = (int) event.getRawY() - lastY;
+
+                    int left = v.getLeft() + dx;
+                    int top = v.getTop() + dy;
+                    int right = v.getRight() + dx;
+                    int bottom = v.getBottom() + dy;
+                    // The flight can not pass over the bound of screen
+                    if (left < 0) {
+                        left = 0;
+                        right = left + v.getWidth();
+                    }
+
+                    if (right > screenWidth) {
+                        right = screenWidth;
+                        left = right - v.getWidth();
+                    }
+
+                    if (top < 0) {
+                        top = 0;
+                        bottom = top + v.getHeight();
+                    }
+
+                    if (bottom > screenHeight) {
+                        bottom = screenHeight;
+                        top = bottom - v.getHeight();
+                    }
+
+                    v.layout(left, top, right, bottom);
+
+                    lastX = (int) event.getRawX();
+                    lastY = (int) event.getRawY();
+
+                    break;
+                case MotionEvent.ACTION_UP:
+                    //check the moving distance to determine if it is a click event
+                    if (Math.abs(event.getRawX() - x) < 10 && Math.abs(event.getRawY() - y) < 10) {
+                        try {
+                            Field field = View.class.getDeclaredField("mListenerInfo");
+                            field.setAccessible(true);
+                            Object object = field.get(v);
+                            field = object.getClass().getDeclaredField("mOnClickListener");
+                            field.setAccessible(true);
+                            object = field.get(object);
+                            if (object != null && object instanceof View.OnClickListener) {
+                                ((View.OnClickListener) object).onClick(v);
+                            }
+                        } catch (Exception e) {
+                        }
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
 }
