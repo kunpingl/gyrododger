d69d6f7bbb46304dddea1f7216dfccbacef79ecd
Kunping Lin <kunping2@illinois.edu>
2019-12-09 22:29:30 -0600

    update: startMenu work for Pixel 3

--- app/src/main/java/com/example/gyrododger/StartActivity.java
+++ app/src/main/java/com/example/gyrododger/StartActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -18,7 +18,7 @@ public class StartActivity extends AppCompatActivity {
         ring.start();
         ring.setLooping(true);
 
-        ImageButton btn_main_play = findViewById(R.id.btn_main_play);
+        Button btn_main_play = findViewById(R.id.btn_main_play);
         btn_main_play.setOnClickListener(unused -> {
             ring.stop();
             startActivityForResult(new Intent(this, GameActivity.class),1);
