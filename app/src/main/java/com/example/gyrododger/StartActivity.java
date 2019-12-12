3fbe80ecf395fbfc2ad25cb80e8044e3b83db2e3
Kunping Lin <kunping2@illinois.edu>
2019-12-12 15:12:55 -0600

    update: version v4.5 new_menu_Ui

--- app/src/main/java/com/example/gyrododger/StartActivity.java
+++ app/src/main/java/com/example/gyrododger/StartActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -18,7 +18,7 @@ public class StartActivity extends AppCompatActivity {
         ring.start();
         ring.setLooping(true);
 
-        Button btn_main_play = findViewById(R.id.btn_main_play);
+        ImageButton btn_main_play = findViewById(R.id.btn_main_play);
         btn_main_play.setOnClickListener(unused -> {
             ring.stop();
             startActivityForResult(new Intent(this, GameActivity.class),1);
